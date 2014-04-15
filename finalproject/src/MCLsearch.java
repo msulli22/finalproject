fee886a93bd90146a8675ab6f3f509f3ac7c2fdb
msulli22 <msulli22@gmail.com>
2014-04-15 18:12:47 -0400

    added system test for Michayla's github

--- finalproject/src/MCLsearch.java
+++ finalproject/src/MCLsearch.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -16,7 +15,7 @@ public class MCLsearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Connection connection;
 	Statement statement = null;
-       
+System.out.println("github michayla test");   
     /**
      * @see HttpServlet#HttpServlet()
      */
